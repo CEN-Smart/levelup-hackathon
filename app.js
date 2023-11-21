7f512d3233940877b51665d07a122df5af9af1cd
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-11-21 11:51:57 +0100

    add new styles

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const toggleBtn = document.querySelector(".drop-down--toggle");
 
 const chevronDown = document.querySelector(".chevron-down");
 const chevronUp = document.querySelector(".chevron-up");
-
+const checked = document.querySelectorAll(".checked");
 toggleBtn.addEventListener("click", () => {
   const content = document.querySelector(".drop-down--content");
   // display content as block or none
-  content.style.display = content.style.display === "block" ? "none" : "block";
+  content.classList.toggle("active");
   // toggle chevron icons
   chevronDown.style.display =
     chevronDown.style.display === "none" ? "block" : "none";
   chevronUp.style.display =
     chevronUp.style.display === "block" ? "none" : "block";
+});
+
+checked.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
 });
